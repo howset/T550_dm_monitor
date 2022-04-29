75cf438ee67d0b7d281a67fb2036ae5f38b6663e
howset <howsetya@googlemail.com>
2022-04-29 11:32:13 +0200

    Force resolution in mono display

--- display_settings.sh
+++ display_settings.sh
@@ -5,6 +5,7 @@ dmode="$(cat /sys/class/drm/card0-DP-1/status)"
 if [ "${dmode}" = disconnected ]
 then 
     #One display
+    xrandr --output eDP-1 --mode 2560x1440
     xfce4-panel --restart
     xfconf-query -c xfwm4 -p /general/margin_right -s 220
     xfconf-query -c xfwm4 -p /general/margin_left -s 10
@@ -16,5 +17,5 @@ else
     xfconf-query -c xfwm4 -p /general/margin_right -s 0
     xfconf-query -c xfwm4 -p /general/margin_left -s 0
     xfconf-query -c xfwm4 -p /general/margin_top -s 60
-    xfconf-query -c xfwm4 -p /general/margin_bottom -s 10
+    xfconf-query -c xfwm4 -p /general/margin_bottom -s 0
 fi
